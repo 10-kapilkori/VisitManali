9be553dce7ad2d88f7002ca42fee8a8ba39bbed4
10-kapilkori <kapilkori1021@gmail.com>
2021-10-02 15:45:23 +0530

    final commit

--- app/src/main/java/com/task/visitmanali/fragments/WeatherFragment.java
+++ app/src/main/java/com/task/visitmanali/fragments/WeatherFragment.java
@@ -1,21 +1,38 @@
 package com.task.visitmanali.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.task.visitmanali.R;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class WeatherFragment extends Fragment {
 
+    @SuppressLint("SetTextI18n")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weather, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_weather, container, false);
+        Calendar calendar = Calendar.getInstance();
+        Format f = new SimpleDateFormat("EEEE");
+        String date = f.format(new Date());
+        String s = calendar.getTime().toString().substring(4, 11);
+
+        TextView tv = view.findViewById(R.id.date);
+        tv.setText(date + " " + s);
+        return view;
     }
 }
